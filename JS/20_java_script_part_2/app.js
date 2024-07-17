cc99a87bd9c88be5adb7aa7691479c587f3b7752
B. Tarun Kumar <btarunkumar7010@gmail.com>
2024-07-17 20:12:59 +0530

    JavaScript practices

---
+++ JS/20_java_script_part_2/app.js
@@ -0,0 +1,9 @@
+// console.log("Heloo world!");
+// console.log("Tarun Kumar");
+// let a = 10;
+// let b = 29;
+// console.log("sum is: ",a+b)
+
+let pencilPrice = 10;
+let eraserPrice = 5;
+console.log(`The total price is ${pencilPrice+eraserPrice} rupees`);
