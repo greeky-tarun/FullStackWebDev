29f5b2615b4a5bcdbc0f8a2a83521ab9cbfe1b70
B. Tarun Kumar <btarunkumar7010@gmail.com>
2024-07-19 01:34:18 +0530

    Started methods

---
+++ JS/21_java_script_part_3/app.js
@@ -0,0 +1,20 @@
+//trim
+
+//we know that password never contains space so if the user will put space it will remove by trim
+let password = prompt("Enter a password");
+let newPassword = password.trim();// here a new string will generate
+console.log(newPassword);
+
+//uppercase and lowercase
+let msg = "Tarun kumar"
+console.log(msg.toUpperCase()); //it will show the msg in upper case
+console.log(msg.toLowerCase()); //it will show the msg in lower case
+
+
+//IndexOf:
+let str = "IloveCoding";
+console.log(str.indexOf("love"))   //1
+console.log(str.indexOf("J"))      //-1 (not found)
+console.log(str.indexOf("o"))      //2(only 1 index)
+
+
